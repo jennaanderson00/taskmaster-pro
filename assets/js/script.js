fca819ee49eeaa8f45f0c113a1f416d93f372038
0ffpudding <jennaanderson00@gmail.com>
2022-03-28 11:57:16 -0500

    added drag and drop features

--- assets/js/script.js
+++ assets/js/script.js
@@ -144,6 +144,65 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
+// sortable list
+$(".card .list-group").sortable({
+  connectWith: $(".card .listgroup"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    var tempArr = [];
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    var arrName = $(this)
+      .attr("id")
+      .replace("list", "");
+    tasks[arrName] = tempArr;
+    saveTasks();
+    console.log(text, date);
+  }
+});
+
+// droppable trash
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolarance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
